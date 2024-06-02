09cec76f24a7b68f2b55e30c6ad8dc6ecd38cddf
Madhav Singh <madhavprakashsingh2003@gmail.com>
2024-06-02 18:51:39 +0530

    Navbar added

--- Client/src/Constants/Navbar/Navbar.jsx
+++ Client/src/Constants/Navbar/Navbar.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo1 from '../../Images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
+import { faHouse, faBriefcase, faBookOpen } from '@fortawesome/free-solid-svg-icons'
+
 
 import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
+        <div id='navbar-main' className='nav'>
             <div className='logo-box'>
-                <img className='logo' src="../../Images/logo.png" alt="logo" />
+                <img id= "nav-logo" className='logo' src={logo1} alt="logo" />
+                <h4>Cloaked</h4>
             </div>
-            <div>
-                <link to ={'/'}>Community</link>
+            <div className='nav-options-mid'>
+                <FontAwesomeIcon icon = {faHouse} style={{ fontSize: '12px' }}/>
+                <Link to ={'/'}>Community</Link>
             </div>
-            <div>
-                <link to ={''}>Jobs</link>
+            <div className='nav-options-mid'>
+                <FontAwesomeIcon icon = {faBriefcase} style={{ fontSize: '12px' }}/>
+                <Link to ={'/jobs'}>Jobs</Link>
             </div>
-            <div>
-                <link to ={''}>Courses</link>
+            <div className='nav-options-mid'>
+                <FontAwesomeIcon icon = {faBookOpen} style={{ fontSize: '12px' }}/>
+                <Link to ={'/courses'}>Courses</Link>
             </div>
-            <div>
-                <link to ={''}>Sign In</link>
+            <div className='nav-reg nav-reg-one login'>
+                <Link to ={'/signin'}>Sign in</Link>
             </div>
-            <div>
-                <link to ={''}>Sign Up</link>
-            </div>         
+            <div className='nav-reg signup'>
+            <Link to='/signup'>
+                <span style={{ color: '#EAEAEA' }}>Sign up</span>
+            </Link>
+            </div>
+  
         </div>
         
     )
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './Constants/Navbar/Navbar.jsx'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,6 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Navbar/>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
