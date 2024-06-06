f7574a915f9d3599c87a9d68bbbf774c16c54d51
Madhav Prakash Singh <madhavprakashsingh2003@gmail.com>
2024-06-07 01:18:27 +0530

    Login and signup page done

--- Client/src/Components/Login/Login.jsx
+++ Client/src/Components/Login/Login.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-import './Login.css'
+import React, { useCallback, useState } from 'react'
+import "./Login.css"
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div>
-            <p>Hello World</p>
-        </div>       
+
+        <div className='login-card'>
+            <img id="login-logo" src='/Images/logo.png' alt="Login" />
+            <h6>Cloaked</h6>
+            <div className='login-form'>
+                <div className='login-sub-from'>
+                    <input className='login-field' onChange={(e) => {setEmail(e.target.value)}} type="text" name='email' placeholder='Enter College Email'/>
+                </div>
+
+                <div className='login-sub-form'>  
+                    <input className='login-field' onChange={(e) => {setPassword(e.target.value)}} name='password' type="text" placeholder='Password'/>
+                </div>
+                <br />
+                <button className='login-btn' type="submit">Sign In</button>
+            </div>
+            <div className='login-post-form'>
+                <div className='post-t1'>Forgot Password?</div>
+                <div className='post-t2'>Sign Up</div>
+            </div>
+        </div>
     )
 }
 
--- Client/src/Components/SignUp/SignUp.jsx
+++ Client/src/Components/SignUp/SignUp.jsx
@@ -1,10 +1,34 @@
-import React from "react";
-import './SignUp.css'
+import React, { useState } from 'react'
 
-const SignUp = () => {
+import "./SignUp.css"
+
+const Signup = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState(""); 
+    const [email, setEmail] = useState("");
+
     return (
-        <p>Hello World</p>
+        <div className='signup-card'>
+            <img id="login-logo" src="/Images/logo.png" alt="Login" />
+            <div className='login-form'>
+                <div className='login-sub-from'>
+                    <input onChange={(e) => {setUsername(e.target.value)}} className='login-field' name='username' type="text" placeholder='Username'/>
+                </div>
+                <div className='login-sub-form'>  
+                    <input onChange={(e) => {setPassword(e.target.value)}} className='login-field' name='password' type="text" placeholder='Password'/>
+                </div>
+                <div className='login-sub-form'>  
+                    <input onChange={(e) => {setEmail(e.target.value)}} className='login-field' name='email' type="text" placeholder='E-mail address'/>
+                </div>
+                <hr />
+                <button className='login-btn' type="submit">Sign Up</button>
+            </div>
+            <br />
+            <div className='signup-post-form, post-t1'>
+                Have an account? Sign In
+            </div>
+        </div>
     )
 }
 
-export default SignUp
+export default Signup
